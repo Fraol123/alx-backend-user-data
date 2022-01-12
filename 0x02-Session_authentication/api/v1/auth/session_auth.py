de786a0c8ce00289b6ca582ef023123410c73c1a
Fraol123 <fraoltesfaye.ft@gmail.com>
2022-01-12 19:35:13 +0300

    session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""API session authentication module"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """ Session Authentication """
