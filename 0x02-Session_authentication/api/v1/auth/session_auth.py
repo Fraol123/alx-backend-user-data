94b0a382d73327dd28d76d0c5b1ec09b4290e037
Fraol123 <fraoltesfaye.ft@gmail.com>
2022-01-12 20:04:43 +0300

    created

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,19 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns User ID based on Session ID """
+
+        if session_id is None or isinstance(session_id, str) is False:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on cookie value """
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
+    
