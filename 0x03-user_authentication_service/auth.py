fd1b2d072ea13c8dda7acd16ca49294c3653b7fb
Fraol123 <fraoltesfaye.ft@gmail.com>
2022-01-18 12:24:51 +0300

    creted

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,3 +13,24 @@ def _hash_password(password: str) -> str:
     """ Takes in string arg, converts to unicode Returns
     salted, hashed pswd as bytesstring"""
     return hashpw(password.encode('utf-8'), gensalt())
+
+from db import DB
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers and returns a new user if email isn't listed"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+        
