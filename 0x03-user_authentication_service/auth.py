f5706feb811edb17495abc37e4554d700e1f30ba
Fraol123 <fraoltesfaye.ft@gmail.com>
2022-01-18 13:02:48 +0300

    delete

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,4 +71,13 @@ class Auth:
             return found_user
         except NoResultFound:
             return None
-        
+
+    def destroy_session(self, user_id: str) -> None:
+            """ Updates user's session_id to None"""
+            if user_id is None:
+                return None
+            try:
+                found_user = self._db.find_user_by(id=user_id)
+                self._db.update_user(found_user.id, session_id=None)
+            except NoResultFound:
+                return None
